85d6eb8636dcda1a8c8b88b0f9dc098686f2e7c6
Lopastudio <patrik.nagy@lopastudio.sk>
2024-03-14 19:59:38 +0100

    a lot

--- HamTrackr/logbook/forms.py
+++ HamTrackr/logbook/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
+    frequency = forms.DecimalField(label='Frequency (kHz)')  # Modify the label
+
     class Meta:
         model = Contact
         fields = ['callsign', 'frequency', 'mode']
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Real email address is required!')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- HamTrackr/logbook/urls.py
+++ HamTrackr/logbook/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.contact_list, name='contact_list'),
+    path('', views.home_page, name='home_page'),
+    path('contact_list/', views.contact_list, name='contact_list'),
     path('log_contact/', views.log_contact, name='log_contact'),
+    path('edit_contact/<int:contact_id>/', views.edit_contact, name='edit_contact'),
+
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("register/", views.signup, name="signup"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
